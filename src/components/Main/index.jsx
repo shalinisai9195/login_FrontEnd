7003e760c439a019fa715376b515827bd58d3740
unknown <shalinisai9195@gmail.com>
2023-06-26 14:26:31 +0530

    first commit

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,23 @@
+import styles from './style.module.css';
+
+
+const Mani = ()=>{
+
+const handleLogout = ()=>{
+    localStorage.removeItem("token");
+    window.location.reload();
+}
+ return(
+      <div className={styles.main_container}>
+          <nav className={styles.navbar}>
+             <h1>Fakebook</h1>
+              <button className={styles.white_btn} onClick={handleLogout}>
+               Logout
+              </button>
+        </nav>
+    </div>
+    );
+
+}
+
+export default Mani;
